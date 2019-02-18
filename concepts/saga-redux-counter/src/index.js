c0c852dff82ec0480927f36e27c9decf2bdb8844
Martin Fabbri <martin.fabbri.a@gmail.com>
2019-02-18 14:33:14 -0800

    Smooth, performant transitions using React Transitions Groups

--- concepts/saga-redux-counter/src/index.js
+++ concepts/saga-redux-counter/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Counter from './Counter'
+import reducer from './reducers'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+(() => {
+  ReactDOM.render(
+      <div>hw</div>,
+      document.getElementById('root')
+  )
+})()
