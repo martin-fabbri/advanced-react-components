4b5e3bf86fce6cf546a6f94169e90805ea18aa6d
Martin Fabbri <martin.fabbri.a@gmail.com>
2019-03-16 23:14:35 -0700

    React Hooks TS - Initial commit.

--- hooks-ts-16-8/src/App.tsx
+++ hooks-ts-16-8/src/App.tsx
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
+export default function App() {
+    const [name, setState] = useState('');
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setState(e.target.value);
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <p>Hello {name}</p>
+          <input type="text" value={name} onChange={handleNameChange}/>
       </div>
     );
-  }
 }
-
-export default App;
