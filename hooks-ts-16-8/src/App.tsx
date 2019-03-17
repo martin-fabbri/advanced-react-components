1be3e6a69d0362dfb11b247a1c25eafc257d75db
Martin Fabbri <martin.fabbri.a@gmail.com>
2019-03-17 12:25:33 -0700

    Use custom hooks.

--- hooks-ts-16-8/src/App.tsx
+++ hooks-ts-16-8/src/App.tsx
@@ -1,14 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { LocaleContext, ThemeContext } from './Context';
 
 export default function App() {
-    const [name, setState] = useState('');
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setState(e.target.value);
-    };
+    const name = useFormField('');
+    const surname = useFormField('');
+    const theme = useContext(ThemeContext);
+    const locale = useContext(LocaleContext);
+    const width = useWindowWidth();
+    useDocumentTitle(`${name.value} ${surname}`);
+
     return (
       <div>
-          <p>Hello {name}</p>
-          <input type="text" value={name} onChange={handleNameChange}/>
+          <p>Hello {name.value} {surname.value}</p>
+          <p>Locale: {locale}</p>
+          <p>Window's width: {width}</p>
+          <input type="text" {...name}/>
+          <br/>
+          <input type="text" {...surname}/>
       </div>
     );
+}
+
+const useFormField = (initialValue: string) => {
+    const [value, setValue] = useState(initialValue);
+    const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
+    return {
+        value,
+        onChange: handleValueChange,
+    }
+}
+
+// custom hook
+const useDocumentTitle = (title: string) => {
+    useEffect(() => {
+        document.title = title;
+    });
+}
+
+// custom hook
+const useWindowWidth = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const handleResize = () => {
+        setWidth(window.innerWidth);
+    };
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    });
+    return width;
 }
---
+++ hooks-ts-16-8/src/Context.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const LocaleContext = React.createContext('en_us');
+export const ThemeContext = React.createContext('dark');
+
+
+
